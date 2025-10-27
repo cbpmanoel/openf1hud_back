93b726b018aab8e8b47ef79234f2a99b4591a544
Manoel Pereira <cbp.manoel@gmail.com>
2025-10-27 20:09:49 -0300

    adds main

---
+++ app/main.py
@@ -0,0 +1,10 @@
+#! /usr/bin python3
+
+def main():
+    """
+    Main entry point for the OpenF1 HUD backend application.
+    """
+    print("Hello, F1 2025!")
+
+if __name__ == "__main__":
+    main()
