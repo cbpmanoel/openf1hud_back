f039d709dc714466a5d32c97d70b76bda86dff41
Manoel Pereira <cbp.manoel@gmail.com>
2025-10-29 01:47:58 -0300

    feat: add module documentation for F1 2025 telemetry data packet structures

---
+++ app/telemetry_ingestor/parser/packets/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module defining the structures for parsing F1 2025 telemetry data packets.
+
+References:
+- EA SPORTS™ F1®25 UDP SPECIFICATION forum comment: https://forums.ea.com/blog/f1-games-game-info-hub-en/ea-sports%E2%84%A2-f1%C2%AE25-udp-specification/12187347
+- Data Output from F1 25 v3 (PDF): https://forums.ea.com/t5/s/tghpe58374/attachments/tghpe58374/f1-games-game-info-hub-en/61/4/Data%20Output%20from%20F1%2025%20v3.pdf
+"""
+
+GAME_VERSION = "2025"
