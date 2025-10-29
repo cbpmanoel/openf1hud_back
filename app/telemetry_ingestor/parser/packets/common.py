7a8651f49f660f08fdf9df84c20a2851d1ff696a
Manoel Pereira <cbp.manoel@gmail.com>
2025-10-29 01:38:05 -0300

    refactor: move PacketID enumeration to common module for better organization

--- app/telemetry_ingestor/parser/packets/common.py
+++ app/telemetry_ingestor/parser/packets/common.py
@@ -1,10 +1,40 @@
 import ctypes
+from enum import IntEnum
+
+
+class PacketID(IntEnum):
+    """
+    Enumeration of packet IDs for F1 2025 telemetry data packets.
+    """
+    MOTION = 0                  # Contains all motion data for player’s car – only sent while player is in control
+    SESSION = 1                 # Data about the session – track, time left
+    LAP_DATA = 2                # Data about all the lap times of cars in the session
+    EVENT = 3                   # Various events that happen during a session
+    PARTICIPANTS = 4            # List of participants in the session, mostly relevant for multiplayer
+    CAR_SETUPS = 5              # Packet detailing car setups for cars in the race
+    CAR_TELEMETRY = 6           # Telemetry data for all cars
+    CAR_STATUS = 7              # Status data for all cars
+    FINAL_CLASSIFICATION = 8    # Final classification confirmation at the end of a race
+    LOBBY_INFO = 9              # Information about players in a multiplayer lobby
+    CAR_DAMAGE = 10             # Damage status for all cars
+    SESSION_HISTORY = 11        # Lap and tyre data for session
+    TYRE_SETS = 12              # Extended tyre set data
+    MOTION_EX = 13              # Extended motion data for player car
+    TIME_TRIAL = 14             # Time Trial specific data
+    LAP_POSITIONS = 15          # Lap positions on each lap so a chart can be constructed
+
+    def __repr__(self):
+        return f"PacketID.{self.name}"
+    
+    def __str__(self):
+        return f"PacketID.{self.name}"
+
 
 class PacketStructureBase(ctypes.LittleEndianStructure):
     """
     Base class for packet structures to provide common functionality.
     """
-    _pack_ = 1
+    _pack_ = 1  # Ensure no padding is added by the compiler
 
     def __repr__(self):
         field_values = ', '.join(f"{field[0]}={getattr(self, field[0])}" for field in self._fields_)
--- app/telemetry_ingestor/parser/parser.py
+++ app/telemetry_ingestor/parser/parser.py
@@ -9,34 +9,8 @@ References:
 from enum import IntEnum
 from .packets.header import PacketHeader, HEADER_SIZE
 from .packets.event_data import PacketEventData
+from .packets.common import PacketID
 
-class PacketID(IntEnum):
-    """
-    Enumeration of packet IDs for F1 2025 telemetry data packets.
-    """
-    MOTION = 0                  # Contains all motion data for player’s car – only sent while player is in control
-    SESSION = 1                 # Data about the session – track, time left
-    LAP_DATA = 2                # Data about all the lap times of cars in the session
-    EVENT = 3                   # Various events that happen during a session
-    PARTICIPANTS = 4            # List of participants in the session, mostly relevant for multiplayer
-    CAR_SETUPS = 5              # Packet detailing car setups for cars in the race
-    CAR_TELEMETRY = 6           # Telemetry data for all cars
-    CAR_STATUS = 7              # Status data for all cars
-    FINAL_CLASSIFICATION = 8    # Final classification confirmation at the end of a race
-    LOBBY_INFO = 9              # Information about players in a multiplayer lobby
-    CAR_DAMAGE = 10             # Damage status for all cars
-    SESSION_HISTORY = 11        # Lap and tyre data for session
-    TYRE_SETS = 12              # Extended tyre set data
-    MOTION_EX = 13              # Extended motion data for player car
-    TIME_TRIAL = 14             # Time Trial specific data
-    LAP_POSITIONS = 15          # Lap positions on each lap so a chart can be constructed
-
-    def __repr__(self):
-        return f"PacketID.{self.name}"
-    
-    def __str__(self):
-        return f"PacketID.{self.name}"
-    
     
 def parse_packet(data: bytes):
     """
