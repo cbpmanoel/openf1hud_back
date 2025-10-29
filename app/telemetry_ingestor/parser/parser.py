69e215b0309a603ca1f3fb2fd248c5600ad96120
Manoel Pereira <cbp.manoel@gmail.com>
2025-10-29 01:50:37 -0300

    refactor: remove outdated module documentation from parser.py

--- app/telemetry_ingestor/parser/parser.py
+++ app/telemetry_ingestor/parser/parser.py
@@ -1,12 +1,3 @@
-"""
-Module defining the structures for parsing F1 2025 telemetry data packets.
-
-References:
-- EA SPORTS™ F1®25 UDP SPECIFICATION forum comment: https://forums.ea.com/blog/f1-games-game-info-hub-en/ea-sports%E2%84%A2-f1%C2%AE25-udp-specification/12187347
-- Data Output from F1 25 v3 (PDF): https://forums.ea.com/t5/s/tghpe58374/attachments/tghpe58374/f1-games-game-info-hub-en/61/4/Data%20Output%20from%20F1%2025%20v3.pdf
-"""
-
-from enum import IntEnum
 from .packets.header import PacketHeader, HEADER_SIZE
 from .packets.event_data import PacketEventData
 from .packets.common import PacketID
