9115fe81faa713dd2d1362571349549f2d3850b5
Manoel Pereira <cbp.manoel@gmail.com>
2025-10-31 04:35:11 -0300

    refactor: rename ParticipantsDataPacket class to PacketParticipantsData for consistency

--- app/telemetry_ingestor/packets_2025/participants_data_packet.py
+++ app/telemetry_ingestor/packets_2025/participants_data_packet.py
@@ -306,7 +306,7 @@ class ParticipantData(PacketStructureBase):
     ]
 
 
-class ParticipantsDataPacket(PacketStructureBase):
+class PacketParticipantsData(PacketStructureBase):
     """
     This is a list of participants in the race. If the vehicle is controlled by AI, then the name will be the
     driver name. If this is a multiplayer game, the names will be the Steam Id on PC, or the LAN name if
