ddd9c25c2524d4a1c799b42a943fe9505ff96bd3
Manoel Pereira <cbp.manoel@gmail.com>
2025-10-31 04:46:18 -0300

    refactor: define the public interface of packet module

--- app/telemetry_ingestor/packets_2025/__init__.py
+++ app/telemetry_ingestor/packets_2025/__init__.py
@@ -6,4 +6,27 @@ References:
 - Data Output from F1 25 v3 (PDF): https://forums.ea.com/t5/s/tghpe58374/attachments/tghpe58374/f1-games-game-info-hub-en/61/4/Data%20Output%20from%20F1%2025%20v3.pdf
 """
 
-GAME_VERSION = "2025"
+
+from .car_telemetry_packet import PacketCarTelemetryData
+from .event_data_packet import PacketEventData
+from .header_packet import PacketHeader
+from .lap_data_packet import PacketLapData
+from .motion_packet import PacketMotionData
+from .participants_data_packet import PacketParticipantsData
+from .session_packet import PacketSessionData
+
+
+GAME_VERSION: str = "2025"
+
+
+__all__ = [
+    "GAME_VERSION",
+    # Packet structures
+    "PacketCarTelemetryData",
+    "PacketEventData",
+    "PacketHeader",
+    "PacketLapData",
+    "PacketMotionData",
+    "PacketParticipantsData",
+    "PacketSessionData",
+]
